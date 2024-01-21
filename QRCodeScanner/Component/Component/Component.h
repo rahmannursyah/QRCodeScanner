5e63b5a63e521acdd363a3a69c46be2e7a1bf3c9
Rahmannur Rizki Syahputra <rahmannur@getgo.sg>
2024-01-21 09:21:11 +0700

    feat: initial commit setting up project

---
+++ QRCodeScanner/Component/Component/Component.h
@@ -0,0 +1,18 @@
+//
+//  Component.h
+//  Component
+//
+//  Created by Rahmannur Rizki Syahputra on 21/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Component.
+FOUNDATION_EXPORT double ComponentVersionNumber;
+
+//! Project version string for Component.
+FOUNDATION_EXPORT const unsigned char ComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Component/PublicHeader.h>
+
+
---
+++ QRCodeScanner/Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Rahmannur Rizki Syahputra on 21/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
